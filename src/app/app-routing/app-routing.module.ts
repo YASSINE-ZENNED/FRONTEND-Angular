0bfa9d859d3a6193a7aaff25dfb32915eb940d36
YASSINE ZENNED <yassine.zenned.123@gmail.com>
2021-11-28 18:46:52 +0100

    routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {RouterModule,Router} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {routes} from './routes';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,23 @@ import {MatButtonModule} from '@angular/material/button';
 import 'hammerjs';
 
 
-
-
-
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import {MatGridListModule} from '@angular/material/grid-list';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
-import {DishService} from './services/dish.service';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+import {DishService} from './services/dish.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -30,7 +34,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +48,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService]  ,
   bootstrap: [AppComponent]
